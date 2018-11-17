d1a2aca8423740465d80882878b22f69db5d6991
AMS777 <alberto@oscuro.es>
2018-11-17 22:25:44 +0100

    Don't create a user again in database when logged in by second time.

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
