66aa94ce9189a7c287b44d936817c9f3d2fdedb7
AMS777 <alberto@oscuro.es>
2018-11-18 17:59:11 +0100

    Fix Google OAuth 'https' on Heroku.

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true // fix 'https' on Heroku
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
