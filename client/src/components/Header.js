ebdaa092d66153c2cd8486acfb01f0d7fcf99302
AMS777 <alberto@oscuro.es>
2018-11-19 20:37:51 +0100

    Change page header content depending on if user is logged in.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="" className="left brand-logo">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a href="">Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
