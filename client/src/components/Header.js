b180e3231d8f235c3d8aad336c4e4d7b67851f88
AMS777 <alberto@oscuro.es>
2018-11-19 16:42:27 +0100

    Css style on page header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href="">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
