38fe48a9f27c74d7fb94cc9fd63fa4092c8954c5
AMS777 <alberto@oscuro.es>
2018-11-23 10:48:11 +0100

    Show user surveys.

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card darken-1" key="survey._id">
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <span style={{ marginRight: '10px' }}>Yes: {survey.yes}</span>
+            <span>No: {survey.no}</span>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.renderSurveys()}</div>;
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchSurveys }
+)(SurveyList);
